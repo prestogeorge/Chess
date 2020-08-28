759eb676ce343c48b619a4a42df422958a700c74
prestogeorge <60333379+prestogeorge@users.noreply.github.com>
2020-08-27 23:03:38 -0400

    initial commit of game

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,9 +1,24 @@
 package Game;
 
+import Board.Board;
+
 public class Game {
     private boolean gameOver;
-
+    private boolean whitesTurn;
+    private Board board;
     public Game() {
+        this.gameOver = false;
+        this.whitesTurn = true;
+    }
 
+    public void start() {
+        while (!gameOver) {
+            gameOver = true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
